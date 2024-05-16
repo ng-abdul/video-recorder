ea78db57402628e858f4a0d0dae861cb721a46a8
Abdul Rahman <abdulrahman.connect@outlook.com>
2024-05-16 22:01:23 +0500

    Update home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,13 @@
 import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
-import { WebcamInitError, WebcamModule, WebcamImage, WebcamUtil, WebcamComponent } from 'ngx-webcam';
+import { WebcamInitError } from 'ngx-webcam';
 import { CommonModule } from '@angular/common';
 import { FormsModule, NgModel } from '@angular/forms';
-import { Observable, Subject } from 'rxjs';
 import { Router } from '@angular/router';
 import { log } from 'node:console';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [WebcamModule, CommonModule, FormsModule],
+  imports: [ CommonModule, FormsModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
   encapsulation:ViewEncapsulation.None
@@ -21,20 +20,8 @@ export class HomeComponent implements OnInit {
   public mediaStream!: MediaStream;
   public videoOptions: MediaTrackConstraints = {};
   public isFrontCameraActive: boolean = true;
-  private trigger: Subject<void> = new Subject<void>()
-  private nextWebcam: Subject<boolean | string> = new Subject<boolean | string>()
-  @ViewChild(WebcamComponent) webcamComponent!: WebcamComponent;
   constructor(public router:Router) {
-    if (typeof navigator !== 'undefined') {
-      WebcamUtil.getAvailableVideoInputs()
-        .then((mediaDevices: MediaDeviceInfo[]) => {
-          this.availableCameras = mediaDevices;
-          this.selectedCamera = this.availableCameras[0];
-        })
-        .catch((err) => {
-          console.error('Error getting available video inputs: ', err);
-        });
-    }
+
   }
 
   ngOnInit(): void {
@@ -78,27 +65,12 @@ export class HomeComponent implements OnInit {
     }
     return null;
   }
- 
-  // takeSnapShot() {
-  //   this.trigger.next()
-  // }
-  // toggleCamera(directionOrDeviceId: boolean | string) {
-  //   this.nextWebcam.next(directionOrDeviceId)
-  // }
-  // get triggerObservable(): Observable<void> {
-  //   return this.trigger.asObservable()
-  // }
-  // get nextWebcamObservable(): Observable<boolean | string> {
-  //   return this.nextWebcam.asObservable()
-  // }
+
   public handleInitError(error: WebcamInitError): void {
     if (error.mediaStreamError && error.mediaStreamError.name === "NotAllowedError") {
       console.warn("Camera access was not allowed by user!");
     }
   }
-
-
-
 
   main = async () => {
     const buttonStart = document.querySelector<HTMLButtonElement>('#startRecording')
@@ -144,9 +116,7 @@ export class HomeComponent implements OnInit {
         videoRecorded!.style.display = 'none'; 
         DownloadRecordedVideobtn!.style.display = 'none'; 
       });
-      console.log("click");
-      
-  
+
      setTimeout(() => {
       mediaRecorder.stop();
       buttonStart.style.display = 'inline-block'; 
