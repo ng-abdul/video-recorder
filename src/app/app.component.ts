d52acb117458f063a89b226d295de14de14d55f0
Abdul Rahman <abdulrahman.connect@outlook.com>
2024-05-19 16:07:06 +0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
